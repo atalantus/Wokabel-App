b3ae178624224117278b7b4f9e2faf8c80847976
Pierre-Louis Suckrow <38440901+pierrelouisSuckrow@users.noreply.github.com>
2018-06-20 08:45:23 +0200

    Home Screen Changes
    Titel Hinzugefügt ist noch sehhhhhhhhhhhhhhhhhhhhhhhhhhhhr hässlich

--- App/app/src/main/java/com/wokabel/app/wokabel/HomeScreen.java
+++ App/app/src/main/java/com/wokabel/app/wokabel/HomeScreen.java
@@ -9,5 +9,8 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen2);
-    }
+        }
+
+
+
 }
