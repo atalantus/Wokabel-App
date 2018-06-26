1a4ce3ee85cc4b648e7946410dc627bc4793de59
Direcx <mel.langfeld@gmx.de>
2018-06-26 21:57:26 +0200

    ViewModel and Activity handling v0.1

---
+++ App/app/src/main/java/com/wokabel/app/wokabel/ViewModels/HomeViewModel.java
@@ -0,0 +1,13 @@
+package com.wokabel.app.wokabel.ViewModels;
+
+import android.arch.lifecycle.ViewModel;
+import android.content.Intent;
+import android.view.View;
+import android.os.Bundle;
+
+public class HomeViewModel extends ViewModel {
+    public void EventOnStartClick(){
+        
+    }
+
+}
