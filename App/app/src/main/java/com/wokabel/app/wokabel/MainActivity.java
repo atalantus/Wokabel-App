070b1ac00e6b3060e71d60ba3a7f974493f8c543
atalantus <contact@atalantus.de>
2018-06-27 20:43:26 +0200

    Revert "ViewModel and Activity handling v0.1"
    This reverts commit 1a4ce3ee85cc4b648e7946410dc627bc4793de59.

--- App/app/src/main/java/com/wokabel/app/wokabel/MainActivity.java
+++ App/app/src/main/java/com/wokabel/app/wokabel/MainActivity.java
@@ -1,9 +1,7 @@
 package com.wokabel.app.wokabel;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void languageSelsctScreen(View view){
-
-        Intent intent = new Intent(this, LanguageSelect.class);
-        startActivity(intent);
     }
 }
