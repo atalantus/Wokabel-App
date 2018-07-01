e3aff7b60a879f3c182f048d0334940bc4c7a24b
atalantus <contact@atalantus.de>
2018-07-01 15:48:55 +0200

    Bug fixing and code improvements

---
+++ App/app/src/main/java/com/wokabel/app/wokabel/services/vocabularyTest/VocabularyTest.java
@@ -0,0 +1,111 @@
+package com.wokabel.app.wokabel.services.vocabularyTest;
+
+import java.util.Random;
+import java.util.ArrayList;
+import com.wokabel.app.wokabel.models.Vocable;
+
+public class VocabularyTest {
+
+    // Die verschiedenen Abfrage Modi
+    public enum Modes {
+        KEY_VALUE,
+        VALUE_KEY
+    }
+
+    // Der Abfrage Modus
+    private Modes mode;
+    private ArrayList<Vocable> randomsorted;
+    private Vocable currentVoc;
+
+    /**
+     * Konstruktor
+     * @param input Die Vokabeln die abgefragt werden sollen
+     * @param testMode Der Modus der Abfrage
+     */
+    public VocabularyTest(ArrayList<Vocable> input, Modes testMode) {
+        mode = testMode;
+        Random rand = new Random();
+        randomsorted = new ArrayList<>();
+        ArrayList<Vocable> a = new ArrayList<>();
+        currentVoc = null;
+        for(int i = 0; i < input.size(); i++){
+            for(int l = 5; l > input.get(i).getLevel()-1; l--){
+                a.add(input.get(i));
+            }
+        }
+
+        while (a.size() > 0){
+            int randnum = rand.nextInt(a.size());
+            randomsorted.add(a.get(randnum));
+            a.remove(randnum);
+        }
+    }
+
+    public String getQuestion() throws Exception {
+        currentVoc = randomsorted.get(0);
+
+        switch (mode) {
+            case KEY_VALUE:
+                return currentVoc.getKey();
+            case VALUE_KEY:
+                ArrayList<String> temp = currentVoc.getValuesList();
+                String ValueString = "";
+
+                for (String s:temp) {
+                    ValueString += s;
+                    ValueString += ", ";
+                }
+
+                return ValueString;
+        }
+
+        throw new Exception("Der Abfrage Modus '" + mode.toString() + "' wurde noch nicht in getQuestion() implementiert!");
+    }
+
+    public String getHelper(){
+        return currentVoc.getHelper();
+    }
+
+    public boolean handleAnswer(String answer) throws Exception {
+        switch (mode) {
+            case KEY_VALUE:
+                ArrayList<String> temp = currentVoc.getValuesList();
+
+                for (String s:temp) {
+                    if(s.equals(answer)){
+                        return true;
+                    }
+                }
+                return false;
+            case VALUE_KEY:
+                return answer.equals(currentVoc.getKey()); //answer is right
+        }
+
+        throw new Exception("Der Abfrage Modus '" + mode.toString() + "' wurde noch nicht in handleAnswer() implementiert!");
+    }
+
+    public String getAnswer() throws Exception {
+        String answer = "";
+
+        switch (mode) {
+            case KEY_VALUE:
+                ArrayList<String> temp = currentVoc.getValuesList();
+
+                for (String s:temp) {
+                    answer += s;
+                    answer += ", ";
+                }
+                break;
+            case VALUE_KEY:
+                answer = currentVoc.getKey();
+                break;
+        }
+
+        if (!answer.equals("")) {
+            randomsorted.remove(0);
+            return answer;
+        }
+
+        throw new Exception("Der Abfrage Modus '" + mode.toString() + "' wurde noch nicht in getAnswer() implementiert!");
+    }
+}
