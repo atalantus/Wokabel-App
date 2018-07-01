6b676c8e48a829740658c806653d98eabeda337e
atalantus <contact@atalantus.de>
2018-07-01 03:23:17 +0200

    Started implementation of SubjectSelect View

--- App/app/src/main/java/com/wokabel/app/wokabel/views/Welcome.java
+++ App/app/src/main/java/com/wokabel/app/wokabel/views/Welcome.java
@@ -1,7 +1,10 @@
 package com.wokabel.app.wokabel.views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.wokabel.app.wokabel.R;
 
@@ -11,5 +14,13 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void start(View view) {
+        EditText forenameInput = findViewById(R.id.forenameInput);
+        String forename = forenameInput.getText().toString();
+        //TODO: Save forename in Settings
+        Intent intent = new Intent(this, SubjectSelect.class);
+        startActivity(intent);
     }
 }
