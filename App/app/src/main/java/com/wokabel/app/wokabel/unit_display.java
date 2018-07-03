8f165753f8937518decb51e51eb5ecc83e056fa6
Pierre-Louis Suckrow <38440901+pierrelouisSuckrow@users.noreply.github.com>
2018-07-03 18:13:46 +0200

    - added Unit_display and unit_select

---
+++ App/app/src/main/java/com/wokabel/app/wokabel/unit_display.java
@@ -0,0 +1,13 @@
+package com.wokabel.app.wokabel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class unit_display extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unit_display);
+    }
+}
