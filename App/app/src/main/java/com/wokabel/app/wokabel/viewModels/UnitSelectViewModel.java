2d14257110ee36f81d588f10388e3757bb15ad39
Direcx <mel.langfeld@gmx.de>
2018-07-11 18:48:20 +0200

    deleted useless code

--- App/app/src/main/java/com/wokabel/app/wokabel/viewModels/UnitSelectViewModel.java
+++ App/app/src/main/java/com/wokabel/app/wokabel/viewModels/UnitSelectViewModel.java
@@ -6,32 +6,21 @@ import android.arch.lifecycle.ViewModel;
 import com.wokabel.app.wokabel.models.Subgroup;
 import com.wokabel.app.wokabel.models.Supergroup;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class UnitSelectViewModel extends ViewModel {
     private LiveData<Supergroup> selectedSupergroup;
-    private LiveData<Subgroup> selectedSubgrooup;
     private LiveData<List<Subgroup>> subgroups;
-    private ArrayList<String> ids;
 
     public UnitSelectViewModel(){
 
         //alle subgroups der selected Supergroup abfragen
         subgroups = null;
-        ids = new ArrayList<>();
-        ids.add("Englisch");
-        ids.add("Französisch");
-        ids.add("toller Test");
     }
 
     public void setSelectedSupergroup(String ID){
         //Abfrage an Datenbank getSupergroupbyId(ID)
         selectedSupergroup = null;
-    }
-
-    public void setSelectedSubgrooup(LiveData<Subgroup> selectedSubgroup) {
-        this.selectedSubgrooup = selectedSubgroup;
     }
 
     public LiveData<List<Subgroup>> getSubgroups() {
