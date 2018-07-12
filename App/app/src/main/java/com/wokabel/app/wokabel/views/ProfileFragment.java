13db63e1d4720dbf649b461b4e21fa1b2d67cb25
Jonas <contact@atalantus.de>
2018-07-12 14:51:47 +0200

    -

--- App/app/src/main/java/com/wokabel/app/wokabel/views/ProfileFragment.java
+++ App/app/src/main/java/com/wokabel/app/wokabel/views/ProfileFragment.java
@@ -21,12 +21,12 @@ public class ProfileFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
         Settings settings = WokabelApplication.sharedPreferences;
         TextView username = view.findViewById(R.id.displayUsername);
         TextView user_xp = view.findViewById(R.id.displayXP);
         username.setText(settings.getString("username"));
-        user_xp.setText(""+settings.getInt("user_xp"));
+        user_xp.setText(settings.getInt("user_xp"));
         return view;
     }
 
