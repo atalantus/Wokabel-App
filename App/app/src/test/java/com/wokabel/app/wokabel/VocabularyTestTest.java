9872b0e4cf40e13413ddc02d4bc89a38ba89055f
Jonas <contact@atalantus.de>
2018-07-12 11:19:53 +0200

    Added test class

---
+++ App/app/src/test/java/com/wokabel/app/wokabel/VocabularyTestTest.java
@@ -0,0 +1,30 @@
+package com.wokabel.app.wokabel;
+
+import com.wokabel.app.wokabel.services.vocabularyTest.VocabularyTest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class VocabularyTestTest {
+
+    @Test
+    public void mode_keyValue() {
+        // TODO: Key -> Value tests
+    }
+
+    @Test
+    public void mode_valueKey() {
+        // TODO: Value -> Key tests
+    }
+
+    @Test
+    public void mode_random() {
+        // TODO: Random tests
+    }
+}
